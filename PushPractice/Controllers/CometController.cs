43cfa33bd12884c4606084efff266a5192a2fe23
鈴木悠也 <monorix1013@live.jp>
2018-08-18 23:53:47 +0900

    プロジェクト ファイルを追加します。

---
+++ PushPractice/Controllers/CometController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PushPractice.Controllers
+{
+    [AsyncTimeout(60000)]
+    public abstract class CometController : AsyncController
+    {
+        private static readonly object _syncLock = new object();
+        private static readonly Dictionary<object, WeakReference> _clients = new Dictionary<object, WeakReference>();
+
+        protected void AddObserver(object key, Action callback)
+        {
+            lock (_syncLock)
+            {
+                var reference = new WeakReference(callback);
+
+                if (_clients.ContainsKey(key))
+                {
+                    _clients[key] = reference;
+                }
+                else
+                {
+                    _clients.Add(key, reference);
+                }
+            }
+        }
+
+        protected void Notify()
+        {
+            lock (_clients)
+            {
+                foreach(var client in _clients){
+                    var reference = client.Value;
+
+                    if (reference.IsAlive)
+                    {
+                        ((Action)reference.Target)?.Invoke();
+                    }
+                }
+            }
+        }
+
+        protected void RemoveObserver(object key)
+        {
+            lock (_syncLock)
+            {
+                _clients.Remove(key);
+            }
+        }
+    }
+}
---
+++ PushPractice/Controllers/TestController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PushPractice.Controllers
+{
+    public class Status
+    {
+        
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Name { get; set; }
+        public List<Status> list { get; set; }
+    }
+    public class TestController : CometController
+    {
+        public static int maxId = 0;
+        public static List<Status> list = new List<Status>();
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var s = new Status()
+            {
+                Id = ++maxId,
+                list = list,
+            };
+            return View(s);
+        }
+        [HttpPost]
+        public ActionResult Create(Status s)
+        {
+            lock (list)
+            {
+                list.Add(s);
+            }
+            Notify();
+            s.list = list;
+            return View(s);
+        }
+        
+        public void ReceiveAsync(int since_id = 0)
+        {
+            if (since_id == 0)
+            {
+                return;
+            }
+            AsyncManager.OutstandingOperations.Increment();
+            AddObserver(since_id, () =>
+             {
+                 AsyncManager.Parameters["since_id"] = since_id;
+                 AsyncManager.OutstandingOperations.Decrement();
+             });
+        }
+
+        public ActionResult ReceiveCompleted(int since_id = 0)
+        {
+            RemoveObserver(since_id);
+
+            var statuses = list.Select(p => new
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Text = p.Text
+            }).ToArray();
+            return Json(statuses);
+        }
+    }
+}
